e481d6101137ad64f3b6f267d802a749eff0a4d8
unknown <arturodiazmay@gmail.com>
2022-08-17 16:18:59 -0500

    added attack() and RunGame() functions

--- main.py
+++ main.py
@@ -10,5 +10,27 @@ def prompt():
     print(f'You are at {hercules_hp} Health and have an attack power of {hercules_ap}')
     for attack in range(len(attack_names)):
         print(f'Please type {attack} if you would like to {attack_names[attack]}')
+    selection = input("Please type your selection here:")
+    return selection
+def attack(current_enemy_hp):
+            # print(f"The enemy is at {enemy_hp} health and you hit him for {hercules_ap} damage")
+            current_enemy_hp = current_enemy_hp - hercules_ap
+            # print(f" The enemy is now at {enemy_hp_afterhit} health")
+            return current_enemy_hp
 
-prompt()
+
+def RunGame():
+    user_input = prompt()
+    if user_input == "1" or "2" or "3" or "0":
+        enemy_dead = False
+        current_enemy_hp = attack(enemy_hp)
+        while enemy_dead == False:
+            print(f'Raid Boss Health:{current_enemy_hp}')
+            if current_enemy_hp != 0:
+             current_enemy_hp = attack(current_enemy_hp)
+            else:
+             print("you have killed the enemy")
+             enemy_dead = True
+            
+        
+RunGame()
