d34ac351dd48637efe0f58c33cd35b21e3b469c3
Arturo Diaz <arturodiazmay@gmail.com>
2024-12-07 19:14:21 -0600

    Feat: Changed Main Combat Loop to Battle Function. Added New Enemies. Added Second Battle.

--- main.py
+++ main.py
@@ -6,67 +6,91 @@ def RunGame():
     hercules_ap = 25
     enemy_hp = 40
     enemy_ap = 15
-    winround1 = False
+    attack_names = ['Sword Slash', 'Shield Bash', "Lunge", "Slide"]
+    continue_battle = False
 
-    #Main Battle Loop
-    while enemy_hp > 0 and hercules_hp > 0:
-        user_input = prompt(hercules_hp, hercules_ap)
-        
-        if user_input in ["0", "1", "2", "3"]:
-            print("You attack the Raid Boss!")
-            enemy_hp = attack(enemy_hp, hercules_ap)
-            
-            if enemy_hp <= 0:
-                print("You have killed the enemy!")
-                winround1 = True
-                break
-            
-            print(f'The enemy attacks you back with {random.choice(attack_names)}')
-            hercules_hp = attackback(hercules_hp, enemy_ap)
-            
-            if hercules_hp <= 0:
-                print("You have been defeated!")
-                break
-            
-            print(f'Your Health: {hercules_hp}')
-            print(f'Raid Boss Health: {enemy_hp}')
-        else:
-            print("Invalid input. Please try again.")
-    if winround1:
+    #Welcome Message to Player
+    print("""You are Hercules, a hero that is being monitored by a highly powerful organization. You have no choice but to obey every living second.
+    That is until you don't. You are captured and en route to be killed. You manage to escape and realize you're inside the enemy base,
+    face to face with one of your arch rivals, HADES! You ready for battle.\n""")
+
+    #Linear Battle Path
+    hercules_hp, hercules_ap, hercules_win = Battle(enemy_hp, enemy_ap, hercules_ap, hercules_hp, attack_names, enemy_name="Hades")
+    if hercules_win:
+        print("Congratulations! You won the battle\n")
         hercules_ap = Uprgade(hercules_ap)
+        print(f"Your new attack power is {hercules_ap}\n")
+        continue_battle = True
+    else:
+        print("Game Over. Better luck next time!")
+    
+    if continue_battle:
+        hercules_hp, hercules_ap, hercules_win = Battle(enemy_hp, enemy_ap, hercules_ap, hercules_hp, attack_names, enemy_name="Hydra")
+        if hercules_win:
+            print('Congratulations, you have Defeated your Rivals! You are the Ultimate Champion!')
+        else:
+             print("Game Over. Better luck next time!")
 
 
-attack_names = ['Sword Slash', 'Shield Bash', "Lunge", "Slide"]
 
-print("""You are Hercules, a hero that is being monitored by a highly powerful organization. You have no choice but to obey every living second.
-That is until you don't. You are captured and en route to be killed. You manage to escape and realize you're inside the enemy base,
-face to face with the leader of the organization.""")
-
-def prompt(hercules_hp, hercules_ap):
+#Attack Prompt
+def prompt(hercules_hp, hercules_ap, attack_names, enemy_name, enemy_hp, enemy_ap):
     print(f'You are at {hercules_hp} Health and have an attack power of {hercules_ap}')
+    print(f'{enemy_name} is at {enemy_hp} Health and has an attack power of {enemy_ap}\n')
     for i in range(len(attack_names)):
         print(f'Please type {i} if you would like to use {attack_names[i]}')
-    selection = input("Please type your selection here: ")
+    selection = input("Please type your selection here: \n")
     return selection
 
-def attack(enemy_hp, hercules_ap):
-    print(f"The enemy is at {enemy_hp} health and you hit him for {hercules_ap} damage.")
+#Player Attack Function
+def attack(enemy_hp, hercules_ap, enemy_name):
+    print(f"{enemy_name} is at {enemy_hp} health and you hit him for {hercules_ap} damage.\n")
     current_enemy_hp = max(0, enemy_hp - hercules_ap)
-    print(f"The enemy is now at {current_enemy_hp} health.")
+    print(f"{enemy_name} is now at {current_enemy_hp} health.\n")
     return current_enemy_hp
 
-def attackback(hercules_hp, enemy_ap):
-    print(f"The enemy hits you for {enemy_ap} damage.")
+#Enemy Attack Function
+def attackback(hercules_hp, enemy_ap, enemy_name):
+    print(f"{enemy_name} hits you for {enemy_ap} damage.\n")
     your_current_hp = max(0, hercules_hp - enemy_ap)
     return your_current_hp
 
+#Upgrade Function
 def Uprgade(hercules_ap):
-    print('After defeating your enemy, you feel a charge of light fill your body, roll the dice to see your fate.' )
+    print('After defeating your enemy, you feel a charge of light fill your body, roll the dice to see your fate.\n' )
     input('type Roll to Roll:')
     dice_roll = random.randint(0, 10) 
     print(f'You rolled a {dice_roll}')
-    if dice_roll <5: print("no updgrade see ya next time!")
-    if dice_roll >5: print("your attack has been increased!")
+    if dice_roll <5: print("no updgrade see ya next time!\n")
+    if dice_roll >5: print("your attack has been increased!\n")
     if dice_roll >5: hercules_ap = hercules_ap* (1 + random.uniform(0, 0.5))
     return hercules_ap
+
+#Battle Function
+def Battle(enemy_hp, enemy_ap, hercules_ap, hercules_hp, attack_names, enemy_name):
+        hercules_win = False
+        enemy_win = False
+        while enemy_hp > 0 and hercules_hp > 0:
+            user_input = prompt(hercules_hp, hercules_ap, attack_names, enemy_name, enemy_hp, enemy_ap)
+            
+            if user_input in ["0", "1", "2", "3"]:
+                print(f"You attack {enemy_name}!\n")
+                enemy_hp = attack(enemy_hp, hercules_ap, enemy_name)
+                
+                if enemy_hp <= 0:
+                    print(f"You have killed {enemy_name}!\n")
+                    return hercules_hp, hercules_ap, True
+                
+                print(f'{enemy_name} attacks you back with {random.choice(attack_names)}\n')
+                hercules_hp = attackback(hercules_hp, enemy_ap, enemy_name)
+                
+                if hercules_hp <= 0:
+                    print("You have been defeated!")
+                    return False
+                
+                print(f'Your Health: {hercules_hp}\n')
+                print(f'{enemy_name} Health: {enemy_hp}\n')
+            else:
+                print("Invalid input. Please try again.")
+    
 RunGame()
